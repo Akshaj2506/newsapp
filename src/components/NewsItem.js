5c00f3b2bca09754972e361120bf8f4c8df07b96
Akshaj2506 <74644487+Akshaj2506@users.noreply.github.com>
2023-07-28 18:43:32 +0530

    populated newsitems by fetching data from API

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -9,7 +9,7 @@ export class NewsItem extends Component {
           <div className="card-body">
             <h5 className="card-title">{title}{(title.length === 70 ? "..." : "")}</h5>
           <p className="card-text">{description}{(description.length >= 80 ? "..." : "")}</p>
-            <a href={newsUrl} target='_blank' className="btn btn-primary">Read More...</a>
+          <a href={newsUrl} rel="noreferrer" target='_blank' className="btn btn-primary">Read More...</a>
           </div>
       </div>
     )
